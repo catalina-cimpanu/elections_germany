2d4984096e59becee0e23c2e67442d0c3bfe1980
catalina <catalina.cimpanu@gmail.com>
2025-11-13 19:23:30 +0100

    added requirements

--- streamlit_app/Home.py
+++ streamlit_app/Home.py
@@ -4,4 +4,3 @@ import plotly.express as px
 st.set_page_config(page_title="Home", layout="wide")
 st.title("🥨 Elections in Germany")
 st.write("Welcome to our app! Here we explore the election results in Germany and different socio-economic parameters over time! \n\nUse the sidebar to navigate to other pages.")
-
