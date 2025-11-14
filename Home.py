db13ebcee482a30be503cc9752de9a37b8b139f6
catalina <catalina.cimpanu@gmail.com>
2025-11-14 14:21:57 +0100

    small modifications

--- Home.py
+++ Home.py
@@ -18,7 +18,8 @@ st.markdown("""
             3. The **emergence of AfD as a major protest party**, with gains concentrated in or after periods of crisis rather than during “normal” growth years.
             4. CDU/CSU (0.74) show a moderate positive correlation, potentially meaning their vote share tends to increase in election years that follow stronger economic conditions. Greens (–0.72) show a moderate negative correlation, indicating their vote share tends to increase in periods following weaker economic performance. And, AfD (–0.34) has a weak-to-moderate negative correlation, meaning their vote gains slightly coincide with periods of weaker economic conditions.
 
-            If you want to see the maps
+            
+            If you want to see the graphs:
 """)
 st.page_link("pages/03_GDP_Growth_and_Elections.py", label="Click here")
 
@@ -30,13 +31,13 @@ st.markdown("""
             - CSU has been very stable over the years in south-east Germany
             - The Greens have been punctually chosen in 2021, mainly in areas with high income, lost popularity again in 2025       
 
-            If you want to see the maps     
+            
+            If you want to see the maps:     
             """)
 st.page_link("pages/04_Elections_and_Income.py", label="Click here")
 
 st.markdown("""
-
-
+            ##### Information about the German political parties
             """)
 
 st.page_link("pages/06_Information_about_the_parties.py", label="Click here to see information about the German political parties")
